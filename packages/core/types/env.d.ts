473a0e8c995b73cb889add6603aabf2d3ad7dc60
无患子 <92346802+jade-gjz@users.noreply.github.com>
2023-06-28 10:36:49 +0800

    Apply suggestions from code review
    Co-authored-by: zwtkito <85665962+zwtkito@users.noreply.github.com>

--- packages/core/types/env.d.ts
+++ packages/core/types/env.d.ts
@@ -1,8 +1,4 @@
 /* eslint-disable no-underscore-dangle */
 /** 当前为开发环境 */
 declare const __DEV__: boolean
-/** 当前为测试环境 */
-declare const __TEST__: boolean
-/** 当前为生产环境 */
-declare const __PROD__: boolean
 /* eslint-enable no-underscore-dangle */
