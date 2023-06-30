ada1527565d43307c9920beeea965802bed7a86d
绝缘子 <jiangang.zhang@jaderd.com>
2023-06-30 18:19:08 +0800

    🐛 fix: test

--- packages/storage/src/vue.ts
+++ packages/storage/src/vue.ts
@@ -40,7 +40,7 @@ export function useLocal<T extends string | number | boolean | object | null>(
   initialValue: MaybeRef<T>,
   crypto = false,
   options: UseStorageOptions<T> = { window: defaultWindow }
-) {
+): RemovableRef<T> {
   return useWindowStorage<T>(localStorage, key, initialValue, crypto, options)
 }
 
@@ -56,6 +56,6 @@ export function useSession<T extends string | number | boolean | object | null>(
   initialValue: MaybeRef<T>,
   crypto = false,
   options: UseStorageOptions<T> = { window: defaultWindow }
-) {
+): RemovableRef<T> {
   return useWindowStorage<T>(sessionStorage, key, initialValue, crypto, options)
 }
