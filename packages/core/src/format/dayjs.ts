5b76b6ecdeb561024d36b350bd56955f875da389
无患子 <92346802+jade-gjz@users.noreply.github.com>
2023-08-04 16:35:35 +0800

    ✨ feat(core): add dayjs (#29)
    * ✨ feat(core): add dayjs
    
    * ♻️ refactor(core/format/dayjs): 重构逻辑
    
    * ✅ test(core/format/dayjs): 补充测试
    
    * 👷 ci: 设定时区为东八区

---
+++ packages/core/src/format/dayjs.ts
@@ -0,0 +1,57 @@
+import type { ConfigType, Dayjs } from 'dayjs/esm'
+import dayjs from 'dayjs/esm'
+import localeData from 'dayjs/esm/plugin/localeData'
+import duration from 'dayjs/esm/plugin/duration'
+import relativeTime from 'dayjs/esm/plugin/relativeTime'
+import customParseFormat from 'dayjs/esm/plugin/customParseFormat'
+import objectSupport from 'dayjs/esm/plugin/objectSupport'
+import zhCN from 'dayjs/esm/locale/zh-cn'
+import { isDef } from '../validate'
+
+dayjs.extend(localeData).locale('zh-cn', zhCN)
+dayjs.extend(duration)
+dayjs.extend(relativeTime)
+dayjs.extend(customParseFormat)
+dayjs.extend(objectSupport)
+
+export { dayjs, Dayjs }
+
+/**
+ * 格式化时间
+ * @param {import('dayjs/esm').ConfigType} date 时间
+ * @param {string} format 格式
+ * @returns {string} 格式化后的时间
+ */
+export const dateFmt = (date: ConfigType, format: string | 'date' | 'datetime' = 'YYYY-MM-DD HH:mm:ss'): string => {
+  const dd = dayjs(date)
+  let fmt = format
+  if (!isDef(date) || !dd.isValid()) return ''
+  if (format === 'date') fmt = 'YYYY-MM-DD'
+  if (format === 'datetime') fmt = 'YYYY-MM-DD HH:mm:ss'
+
+  return dd.format(fmt)
+}
+
+/**
+ * 格式化时间间隔
+ * @param {import('dayjs/esm').ConfigType} start 开始时间
+ * @param {import('dayjs/esm').ConfigType} end 结束时间
+ */
+export const dateDuration = (start: ConfigType, end: ConfigType) => {
+  const startDay = dayjs(start)
+  const endDay = dayjs(end)
+  if (!isDef(start) || !startDay.isValid() || !isDef(end) || !endDay.isValid()) return ''
+
+  const dd = dayjs.duration(endDay.diff(startDay))
+
+  const year = dd.years()
+  const month = dd.months()
+  const day = dd.days()
+  const format = [
+    year === 0 ? '' : 'Y年',
+    month === 0 ? '' : 'M个月',
+    day === 0 ? '' : `${day < 10 && (year !== 0 || month !== 0) ? '零' : ''}D天`,
+  ].join('')
+
+  return dd.format(format)
+}
--- packages/core/tests/format.test.ts
+++ packages/core/tests/format.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import { filterObj, parseToJSON, replacer, reviver } from '../src'
+import { dateDuration, dateFmt, dayjs, filterObj, parseToJSON, replacer, reviver } from '../src'
 
 test('parseToJSON', () => {
   expect(parseToJSON('')).toStrictEqual(null)
@@ -31,4 +31,32 @@ test('filterObj', () => {
   expect(filterObj({ a: 1, b: 2, c: 3 }, ['a', 'b', 'd'])).toStrictEqual({ a: 1, b: 2 })
   expect(filterObj({ a: 1, b: 2, c: 3 }, ['d'])).toStrictEqual({})
   expect(filterObj({ a: 1, b: 2, c: 3 }, [], true)).toStrictEqual({ a: 1, b: 2, c: 3 })
+})
+
+test('dateFmt', () => {
+  expect(dateFmt(null)).toBe('')
+  expect(dateFmt(undefined)).toBe('')
+  expect(dateFmt('')).toBe('')
+  expect(dateFmt(new Date('2020-01-01'))).toBe('2020-01-01 08:00:00')
+  expect(dateFmt(dayjs('2020-01-01'))).toBe('2020-01-01 00:00:00')
+  expect(dateFmt('2020-01-01')).toBe('2020-01-01 00:00:00')
+  expect(dateFmt('2020-01-01 00:00:00')).toBe('2020-01-01 00:00:00')
+  expect(dateFmt('2020-01-01 08:00:00')).toBe('2020-01-01 08:00:00')
+  expect(dateFmt('2020-01-01 08:00:00', 'YYYY-MM-DD')).toBe('2020-01-01')
+  expect(dateFmt('2020-01-01', 'YYYY-MM-DD HH:mm:ss')).toBe('2020-01-01 00:00:00')
+  expect(dateFmt('2020-01-01', 'YYYY-MM')).toBe('2020-01')
+  expect(dateFmt('2020-01-01 08:00:00', 'date')).toBe('2020-01-01')
+  expect(dateFmt('2020-01-01', 'datetime')).toBe('2020-01-01 00:00:00')
+})
+
+test('dateDuration', () => {
+  expect(dateDuration(null, null)).toBe('')
+  expect(dateDuration(undefined, undefined)).toBe('')
+  expect(dateDuration('2020', undefined)).toBe('')
+  expect(dateDuration('1998-05-23', '2020-01-01')).toBe('21年7个月18天')
+  expect(dateDuration('2019-10-29', '2020-01-01')).toBe('2个月零4天')
+  expect(dateDuration('2019-12-21', '2020-01-01')).toBe('11天')
+  expect(dateDuration('2019-12-29', '2020-01-01')).toBe('3天')
+  expect(dateDuration('2018-12-29', '2020-01-01')).toBe('1年零3天')
+  expect(dateDuration('2018-01-01', '2020-01-01')).toBe('2年')
 })
