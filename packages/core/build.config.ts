c700e50bf6bea6780c173f8fc04c3ad56a5c74ed
绝缘子 <jiangang.zhang@jaderd.com>
2023-08-08 16:50:09 +0800

    fix(core): format/dayjs plugins reference

--- packages/core/build.config.ts
+++ packages/core/build.config.ts
@@ -1,4 +1,12 @@
+import { readFile, writeFile } from 'node:fs/promises'
 import { defineBuildConfig } from 'unbuild'
+
+const dayjsReferences = `/// <reference types="dayjs/esm/plugin/localeData" />
+/// <reference types="dayjs/esm/plugin/duration" />
+/// <reference types="dayjs/esm/plugin/relativeTime" />
+/// <reference types="dayjs/esm/plugin/customParseFormat" />
+/// <reference types="dayjs/esm/plugin/objectSupport" />
+`
 
 export default defineBuildConfig({
   entries: ['src/index'],
@@ -6,5 +14,15 @@ export default defineBuildConfig({
   declaration: true,
   rollup: {
     emitCJS: true,
+  },
+  hooks: {
+    'build:done': ctx => {
+      readFile(`${ctx.options.outDir}/index.d.ts`, 'utf-8').then(content => {
+        writeFile(`${ctx.options.outDir}/index.d.ts`, dayjsReferences + content).then(() => {
+          // eslint-disable-next-line no-console
+          console.log('dayjs references added')
+        })
+      })
+    },
   },
 })
