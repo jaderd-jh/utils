210ecb91f9b59dc89b1636f5b97a5cb38aea87fc
绝缘子 <jiangang.zhang@jaderd.com>
2023-09-06 14:47:21 +0800

    fix: dayjs definition path

--- packages/core/build.config.ts
+++ packages/core/build.config.ts
@@ -1,11 +1,11 @@
 import { readFile, writeFile } from 'node:fs/promises'
 import { defineBuildConfig } from 'unbuild'
 
-const dayjsReferences = `/// <reference types="dayjs/esm/plugin/localeData" />
-/// <reference types="dayjs/esm/plugin/duration" />
-/// <reference types="dayjs/esm/plugin/relativeTime" />
-/// <reference types="dayjs/esm/plugin/customParseFormat" />
-/// <reference types="dayjs/esm/plugin/objectSupport" />
+const dayjsReferences = `/// <reference types="dayjs/plugin/localeData" />
+/// <reference types="dayjs/plugin/duration" />
+/// <reference types="dayjs/plugin/relativeTime" />
+/// <reference types="dayjs/plugin/customParseFormat" />
+/// <reference types="dayjs/plugin/objectSupport" />
 `
 
 export default defineBuildConfig({
