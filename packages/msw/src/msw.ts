751c60ee74d2915565b13ff864e8fafd0cfa0e71
绝缘子 <jiangang.zhang@jaderd.com>
2023-10-24 15:48:11 +0800

    refactor(msw): migrate to v2.0.0

--- packages/msw/src/msw.ts
+++ packages/msw/src/msw.ts
@@ -1,49 +1,45 @@
-import type { MockedResponse, ResponseFunction, rest } from 'msw'
-import { context, createResponseComposition } from 'msw'
-import { fakeIntRange } from '@jhqn/utils-faker'
-import type { MaybeFn } from '../types'
+import type { StrictResponse, http } from 'msw'
+import { HttpResponse, delay } from 'msw'
+import type { MaybeFn, PageRes, Res } from '@jhqn/utils'
+import { fakeIntRange } from '@jhqn/utils'
 
 export const getMockData = <T>(data: T, code = 200, message = 'success') => {
-  return { data, code, message }
+  return { data, code, message } as Res<T>
 }
 
 export const getMockDataList = <T>(data: T[], total: number, code = 200, message = 'success') => {
-  return { data: { records: data, total }, code, message }
+  return { data: { records: data, total }, code, message } as PageRes<T>
 }
 
-export const commonSuccessDelayedRes: ResponseFunction = createResponseComposition({}, [
-  context.status(200),
-  context.delay(fakeIntRange(100, 1000)),
-])
+const randomDelay = () => delay(fakeIntRange(100, 1000))
 
 const fnRes = <T>(fn: MaybeFn<T>) => (fn instanceof Function ? fn() : fn)
 
-export const commonRes: <T>(
-  fn: MaybeFn<T>,
-  ...args: Parameters<Parameters<typeof rest.get | typeof rest.post | typeof rest.put | typeof rest.delete>[1]>
-) => Promise<MockedResponse<any>> | MockedResponse<any> = (fn, ...args) =>
-  commonSuccessDelayedRes(args[2].json(getMockData(fnRes(fn))))
+export const commonRes: <T>(fn: MaybeFn<T>) => Promise<StrictResponse<Res<T>>> = async fn => {
+  await randomDelay()
+  return HttpResponse.json(getMockData(fnRes(fn)))
+}
 
 export const commonArrRes: <T>(
   fn: MaybeFn<T>,
-  range: [min: number, max: number],
-  ...args: Parameters<Parameters<typeof rest.get | typeof rest.post | typeof rest.put | typeof rest.delete>[1]>
-) => Promise<MockedResponse<any>> | MockedResponse<any> = (fn, range, ...args) =>
-  commonSuccessDelayedRes(
-    args[2].json(getMockData(Array.from({ length: fakeIntRange(range[0], range[1]) }, () => fnRes(fn))))
-  )
+  range: [min: number, max: number]
+) => Promise<StrictResponse<Res<T[]>>> = async (fn, range) => {
+  await randomDelay()
+  return HttpResponse.json(getMockData(Array.from({ length: fakeIntRange(range[0], range[1]) }, () => fnRes(fn))))
+}
 
 export const commonPageRes: <T>(
   fn: MaybeFn<T>,
-  ...args: Parameters<Parameters<typeof rest.get>[1]>
-) => Promise<MockedResponse<any>> | MockedResponse<any> = (fn, ...args) => {
+  resolver: Parameters<Parameters<typeof http.get>[1]>[0]
+) => Promise<StrictResponse<PageRes<T>>> = async (fn, resolver) => {
+  await randomDelay()
   const total = 45
-  const page = Number(args[0].url.searchParams.get('page'))
-  const count = Number(args[0].url.searchParams.get('count'))
+  const page = Number(resolver.params.page)
+  const count = Number(resolver.params.count)
   let len = count
   if (page * count > total) {
     len = total - (page - 1) * count
   }
   const data = Array.from({ length: len }, () => fnRes(fn))
-  return commonSuccessDelayedRes(args[2].json(getMockDataList(data, total)))
+  return HttpResponse.json(getMockDataList(data, total))
 }
