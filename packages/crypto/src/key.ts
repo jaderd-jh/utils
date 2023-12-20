21aeee674870560b12c16aef6600ecfa34bdfaf3
绝缘子 <jiangang.zhang@jaderd.com>
2023-12-20 16:52:41 +0800

    feat: support rspack/webpack

--- packages/crypto/src/key.ts
+++ packages/crypto/src/key.ts
@@ -1,20 +1,21 @@
 import encUTF8 from 'crypto-js/enc-utf8'
 
-let KEY: CryptoJS.lib.WordArray
+// eslint-disable-next-line no-underscore-dangle
+let __CRYPTO_KEY__: CryptoJS.lib.WordArray
 
 /**
  * 设置加密key
  * @param key
  */
 export function setCryptoKey(key: string) {
-  KEY = encUTF8.parse(key)
+  __CRYPTO_KEY__ = encUTF8.parse(key)
 }
 
 /**
  * 获取加密key
  */
 export function getCryptoKey() {
-  return KEY
+  return __CRYPTO_KEY__
 }
 
 if (import.meta.env?.VITE_CRYPTO_KEY) {
