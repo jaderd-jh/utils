89aa82d22153cf01db859aab9d789dbafc4e2af7
pigmon <jiangang.zhang@jaderd.com>
2024-05-16 17:18:26 +0800

    fix(deps): update deps and add oxlint & biome

--- .eslintrc.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: '@jhqn/eslint-config-ts',
-  rules: {
-    'no-bitwise': 'off',
-  }
-}
---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import jhqn from '@jhqn/eslint-config'
+import oxlint from 'eslint-plugin-oxlint'
+
+export default jhqn(
+  {
+    lessOpinionated: true,
+    stylistic: {
+      overrides: {
+        'style/indent': 'off',
+        'style/multiline-ternary': 'off',
+        'style/operator-linebreak': 'off',
+      },
+    },
+  },
+  oxlint.configs['flat/recommended']
+)
