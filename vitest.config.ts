9d70e0dca068c040ca2dce3daf257a611da34f1f
pigmon <jiangang.zhang@jaderd.com>
2024-09-30 12:01:57 +0800

    fix: vitest coverage set includes & excludes

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
   test: {
     coverage: {
       provider: 'v8', // or 'istanbul'
+      include: ['**/src/**'],
+      exclude: ['**/dist/**', '**/node_modules/**'],
     },
     environment: 'jsdom',
     globals: true,
