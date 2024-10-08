35e0cebcd0505ed062a41640fde7cb539abae568
pigmon <jiangang.zhang@jaderd.com>
2024-10-08 16:05:27 +0800

    fix(core/validate): extract regexes to top

--- packages/core/src/validate/form.ts
+++ packages/core/src/validate/form.ts
@@ -4,6 +4,9 @@ import { isDef } from './core'
 const cnPhoneRE = /^1[3-9]\d{9}$/
 const cnTelRE = /^0\d{9,11}$/
 const cnUSCIRE = /^[0-9A-HJ-NPQRTUWXY]{2}\d{6}[0-9A-HJ-NPQRTUWXY]{10}$/
+const cnIdCard15Re = /^[1-9]\d{7}(?:0[1-9]|10|11|12)(?:[0-2][1-9]|10|20|30|31)\d{2}[\dX]$/i
+const cnIdCard18Re = /^[1-9]\d{5}(?:18|19|20)\d{2}(?:0[1-9]|1[0-2])(?:[0-2][1-9]|10|20|30|31)\d{3}[\dX]$/i
+const emailRE = /^[a-z\d](?:\w|-)+@[a-z\d]+\.[a-z]{2,5}$/i
 
 /**
  * 是否是[统一社会信用代码](https://zh.wikipedia.org/zh-cn/%E7%BB%9F%E4%B8%80%E7%A4%BE%E4%BC%9A%E4%BF%A1%E7%94%A8%E4%BB%A3%E7%A0%81)
@@ -43,8 +46,7 @@ export const isTel = (val: UnDef<Numeric>) => {
  */
 export const isEmail = (val: UnDef<string>) => {
   if (!isDef(val)) return false
-  const reg = /^[a-z\d](?:\w|-)+@[a-z\d]+\.[a-z]{2,5}$/i
-  return reg.test(val)
+  return emailRE.test(val)
 }
 
 /**
@@ -54,9 +56,7 @@ export const isEmail = (val: UnDef<string>) => {
  */
 export const isIdCard = (val: UnDef<string>, compatible = true) => {
   if (!isDef(val)) return false
-  const card15 = /^[1-9]\d{7}(?:0[1-9]|10|11|12)(?:[0-2][1-9]|10|20|30|31)\d{2}[\dX]$/i
-  const card18 = /^[1-9]\d{5}(?:18|19|20)\d{2}(?:0[1-9]|1[0-2])(?:[0-2][1-9]|10|20|30|31)\d{3}[\dX]$/i
-  return card18.test(val) || (compatible ? card15.test(val) : false)
+  return cnIdCard18Re.test(val) || (compatible ? cnIdCard15Re.test(val) : false)
 }
 
 /**
